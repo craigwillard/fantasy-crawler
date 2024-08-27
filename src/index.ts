b964b96fd521b31c6415a17e55b9576cc725126d
Craig Willard <craigwillard@gmail.com>
2024-08-27 13:37:41 -0400

    Promise-based fetch for all positions

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,48 @@ import * as cheerio from "cheerio";
 import { Players } from "./types/player";
 import { HarrisFootball } from "./sources/harris";
 
-const crawl = async () => {
-  const response = await fetch(HarrisFootball.urls.qb);
-  const htmlBody = await response.text();
+const MAX_PLAYERS = 80;
 
-  const parsedBody = cheerio.load(htmlBody);
-  const table = parsedBody("table:nth-of-type(1) tr");
-  const collection: Players = [];
+async function fetchRanksConcurrently() {
+  const positions = Object.keys(HarrisFootball.urls);
+  const urls = Object.values(HarrisFootball.urls);
+  const responses = await Promise.allSettled(
+    urls.map((url) => fetch(url).then((res) => res.text()))
+  );
 
-  table.each((index, element) => {
-    const rank = index + 1;
-    const name = parsedBody(element).find("td:nth-of-type(2)").text();
-    const team = parsedBody(element).find("td:nth-of-type(3)").text();
-    if (!!name) {
-      collection.push({
-        rank,
-        name,
-        team,
+  responses.forEach((response, index) => {
+    if (response.status === "fulfilled") {
+      const body = cheerio.load(response.value);
+      const table = body(".sqs-layout table:first-child tr");
+      const collection: Players = [];
+      let rank = 0;
+
+      table.each((index, element) => {
+        const pageRank = body(element).find("td:nth-of-type(1)").text();
+        const name = body(element).find("td:nth-of-type(2)").text();
+        const team = body(element).find("td:nth-of-type(3)").text();
+        if (!!pageRank && index - 1 < MAX_PLAYERS) {
+          rank += 1;
+          collection.push({
+            rank,
+            name,
+            team,
+          });
+          console.log(`${rank}.) ${name} - ${team}`);
+        }
       });
-    }
-    console.log(`${rank}.) ${name} - ${team}`);
-  });
-  console.log(collection);
 
-  var json = JSON.stringify(collection, null, 4);
-  fs.writeFile("export/harris-qb.json", json, (err) => {
-    if (err) {
-      console.error(err);
+      var json = JSON.stringify(collection, null, 4);
+      fs.writeFile(`export/harris-${positions[index]}.json`, json, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      // console.log(`Content of ${urls[index]}:`, response.value);
+    } else {
+      console.error(`Error fetching ${urls[index]}:`, response.reason);
     }
   });
-};
+}
 
-crawl();
+fetchRanksConcurrently();
